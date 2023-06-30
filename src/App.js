9f59e9c7d8183acdd4195fb4cbf1be513527dc32
Parag Chauhan <paragchauhan33@gmail.com>
2023-06-30 14:27:21 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+// import Todo from "./ToDoList";
+import TodoListApp from "./ToDoList1";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoListApp />
+
+    </div>
+  );
+}
+
+export default App;
